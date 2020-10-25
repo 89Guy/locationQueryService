0111fca21fc71283eced7513c62593274758ea89
Atul upadhyay <upadhyayatul812@gmail.com>
2020-10-26 00:49:20 +0530

    Test

--- src/test/java/Runner/MainRunner.java
+++ src/test/java/Runner/MainRunner.java
@@ -9,7 +9,8 @@ import org.testng.annotations.AfterSuite;
 @CucumberOptions (
    features = {"src/test/java/FeatureFiles/"},
    glue = {"StepDefinition"},
-   monochrome = true, dryRun = false,
+   monochrome = true,
+        dryRun = false,
    //tags = {"@tag1"},
    plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json", "com.cucumber.listener.ExtentCucumberFormatter:target/report.html"}
    )
